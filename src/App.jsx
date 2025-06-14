337245b2ebdcd9e49b57c6de264ab824a95df103
lah-deh <succssasokere@gmail.com>
2025-06-13 22:09:30 -1100

    worked on the responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
         name="Asokere Success"
         bio={
           <>
-            I'm an Electronics and Computer Engineering student. <br />
-            also a Frontend developer in training who loves building and learning new things.
+            I'm an Electronics and Computer Engineering student
+            and a Frontend developer<br></br> in training who loves building and learning new things.
           </>
         }
       />
